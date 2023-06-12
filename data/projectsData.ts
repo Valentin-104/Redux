a3049f63025d82458bf9d6c88ebcf981d4fa53ac
Valentin1495 <newface95@icloud.com>
2023-06-12 23:10:57 +0900

    Add posts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,7 +3,7 @@ const projectsData = [
     title: 'NFT Trade Shop',
     description: `NFT 발행 및 거래부터, 원하는 코인 구매까지 한 곳에서 모든 것이 가능한 ONE-STOP 가상 NFT 거래소`,
     imgSrc: '/static/images/nft.png',
-    href: '/blog/nft-trade-shop',
+    href: '/blog/projects/NFT-Trade-Shop',
   },
 ]
 
