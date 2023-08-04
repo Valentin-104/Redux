7ed80e0d9014a1981404ac56be0d946ef084f481
Valentin1495 <newface95@icloud.com>
2023-08-05 01:25:56 +0900

    Change logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,27 +1,17 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
+import Logo from '@/data/logo.png'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
+import Image from 'next/image'
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between py-10">
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
-              </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
+            <Image src={Logo} alt='Logo' width={70} height={70} className='rounded-lg shadow-lg' />
         </Link>
       </div>
       <div className="flex items-center text-base leading-5">
