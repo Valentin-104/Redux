9e92922efc4fd26d70339fff9c9d5ac75bb20f42
Valentin1495 <newface95@icloud.com>
2024-02-01 17:56:46 +0900

    Add and edit posts

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,21 +1,21 @@
 const projectsData = [
   {
     title: 'Gemini',
-    description: `Talk to AI & Generate images from AI`,
+    description: `The best AI tool for chatbot/image generation`,
     imgSrc: '/static/images/openai.jpg',
     href: '/blog/projects/Gemini',
-  },
-  {
-    title: 'NFT Trade Shop',
-    description: `NFT 발행 및 거래부터, 원하는 코인 구매까지 한 곳에서 모든 것이 가능한 ONE-STOP 가상 NFT 거래소`,
-    imgSrc: '/static/images/nft.png',
-    href: '/blog/projects/NFT-Trade-Shop',
   },
   {
     title: 'NYTimes',
     description: `Comprehensive up-to-date news coverage, aggregated by New York Times API`,
     imgSrc: '/static/images/new-york-times.jpg',
     href: '/blog/projects/NYTimes',
+  },
+  {
+    title: 'NFT Trade Shop',
+    description: `NFT 발행 및 거래부터, 원하는 코인 구매까지 한 곳에서 모든 것이 가능한 ONE-STOP 가상 NFT 거래소`,
+    imgSrc: '/static/images/nft.png',
+    href: '/blog/projects/NFT-Trade-Shop',
   },
   // {
   //   title: 'AIWanderlust',
