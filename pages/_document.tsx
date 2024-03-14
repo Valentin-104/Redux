a385269b470b8649b3ab32461fa398913e42c774
Valentin1495 <newface95@icloud.com>
2024-03-15 01:41:28 +0900

    Remove favicon - delete favicon.svg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,9 +19,9 @@ class MyDocument extends Document {
             sizes="16x16"
             href="/static/favicons/favicon-16x16.png"
           /> */}
-          <link rel="icon" href="/static/favicons/favicon.svg" />
+          {/* <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" /> */}
+          {/* <link rel="icon" href="/static/favicons/favicon.png" /> */}
           <link rel="manifest" href="/static/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
