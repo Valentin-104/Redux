08bc1a83ba7f573c01498c35d79c6d18ceed52f5
Valentin1495 <newface95@icloud.com>
2024-05-19 18:17:57 +0900

    Edit projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,16 +12,16 @@ const projectsData = [
     href: '/blog/projects/Gemini',
   },
   {
-    title: 'NYTimes',
-    description: `뉴욕타임스 API를 활용한 라이브 뉴스 앱`,
-    imgSrc: '/static/images/new-york-times.jpg',
-    href: '/blog/projects/NYTimes',
-  },
-  {
     title: 'NFT Trade Shop',
     description: `NFT 발행 및 거래부터, 원하는 코인 구매까지 한 곳에서 모든 것이 가능한 ONE-STOP 가상 NFT 거래소`,
     imgSrc: '/static/images/nft.png',
     href: '/blog/projects/NFT-Trade-Shop',
+  },
+  {
+    title: 'NYTimes',
+    description: `뉴욕타임스 API를 활용한 라이브 뉴스 앱`,
+    imgSrc: '/static/images/new-york-times.jpg',
+    href: '/blog/projects/NYTimes',
   },
   {
     title: 'Coding Interview Prep',
