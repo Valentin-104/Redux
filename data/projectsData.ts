8b70d3348a88dfc071a0cb74ef41554e8ee882a5
Valentin1495 <newface95@icloud.com>
2024-10-24 14:16:39 +0900

    Remove project link

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,28 +1,28 @@
 const projectsData = [
-  {
-  title: 'PostPlanet',
-  description: `소셜 미디어 플랫폼`,
-  imgSrc: '/static/images/postplanet.jpg',
-  href: '/blog/projects/PostPlanet',
-  },
-  {
-    title: 'Gemini',
-    description: `AI를 활용한 챗/이미지 생성 앱`,
-    imgSrc: '/static/images/openai.jpg',
-    href: '/blog/projects/Gemini',
-  },
-  {
-    title: 'WhisperWave',
-    description: `실시간으로 메시지를 주고받는 그룹 챗 애플리케이션`,
-    imgSrc: '/static/images/white-paper-plane.jpg',
-    href: '/blog/projects/WhisperWave',
-  },
-  {
-    title: 'NFT Trade Shop',
-    description: `NFT 발행 및 거래부터, 원하는 코인 구매까지 한 곳에서 모든 것이 가능한 ONE-STOP 가상 NFT 거래소`,
-    imgSrc: '/static/images/nft.png',
-    href: '/blog/projects/NFT-Trade-Shop',
-  },
+  // {
+  // title: 'PostPlanet',
+  // description: `소셜 미디어 플랫폼`,
+  // imgSrc: '/static/images/postplanet.jpg',
+  // href: '/blog/projects/PostPlanet',
+  // },
+  // {
+  //   title: 'Gemini',
+  //   description: `AI를 활용한 챗/이미지 생성 앱`,
+  //   imgSrc: '/static/images/openai.jpg',
+  //   href: '/blog/projects/Gemini',
+  // },
+  // {
+  //   title: 'WhisperWave',
+  //   description: `실시간으로 메시지를 주고받는 그룹 챗 애플리케이션`,
+  //   imgSrc: '/static/images/white-paper-plane.jpg',
+  //   href: '/blog/projects/WhisperWave',
+  // },
+  // {
+  //   title: 'NFT Trade Shop',
+  //   description: `NFT 발행 및 거래부터, 원하는 코인 구매까지 한 곳에서 모든 것이 가능한 ONE-STOP 가상 NFT 거래소`,
+  //   imgSrc: '/static/images/nft.png',
+  //   href: '/blog/projects/NFT-Trade-Shop',
+  // },
 ]
 
 export default projectsData
